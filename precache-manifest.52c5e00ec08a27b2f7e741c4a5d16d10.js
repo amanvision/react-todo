b9a19071c08d2529ef2a7febaaf30c9112ee227e
Aman <aman@netzoptimize.com>
2024-05-23 16:26:11 +0530

    Updates

---
+++ precache-manifest.52c5e00ec08a27b2f7e741c4a5d16d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e46bbcf6df807a3a77d4e5f087feb2",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "31ea072f223cf240494f",
+    "url": "/React-Todo-List/static/css/main.b5409bd1.chunk.css"
+  },
+  {
+    "revision": "81e03dc742ae3af532a8",
+    "url": "/React-Todo-List/static/js/2.78835be4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React-Todo-List/static/js/2.78835be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ea072f223cf240494f",
+    "url": "/React-Todo-List/static/js/main.e8f093a2.chunk.js"
+  },
+  {
+    "revision": "10404ab82272844e8a6c",
+    "url": "/React-Todo-List/static/js/runtime-main.3c559431.js"
+  }
+]);
